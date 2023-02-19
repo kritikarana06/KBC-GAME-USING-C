2e237d57349002ca4cb1746546dc1e09e004f3bc
Kritika Rana <112557173+kritikarana06@users.noreply.github.com>
2023-02-19 19:31:29 +0530

    Add files via upload

---
+++ kbc.c
@@ -0,0 +1,133 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    int c;
+    printf("KON BANEGA CROREPATI\n");
+    printf("Swagat hai apka KBC mei\n");
+    printf("pehla sawal 1,000 rs k liye apki screen par ye raha\n");
+    printf("...................................................................\n");
+    printf("Question 1-->The smallest country is the world is?\n");
+    printf("and your options are\n");
+    printf(".....................................................................\n");
+    printf("1)Vetican city\n2)Ghana\n3)Monaco\n4)Maldives\n");
+    scanf("%d",&c);
+    switch(c)
+    {
+        case 2:
+        case 3:
+        case 4:
+        printf("GALAT UTTAR\n");
+        break;
+
+        case 1:
+        printf("SAHI JAWAB\n");
+        printf("You have won 1,000 rupees\n");
+        break;
+        default:printf("/ninvalid ");
+        break;
+        
+    }
+    printf("---*----*----*---*--------*--------*-------*---------*---------*----------------*_\n");
+    printf("2nd queation 2,000 rs k liye apki screen par ye raha\n");
+    printf("..............................................................................................\n");
+    printf("Question 2-->Which is the smallest state of India in terms of population?\n");
+    printf("and your options are\n");
+    printf("..........................................................................................\n");
+    printf("A)GOA\nB)SIKKHIM\nC)TRIPURA\nD)ARUNACHAL PRADESH\n");
+    scanf("%d",&c);
+    switch(c)
+    {
+        case 1:
+        case 3:
+        case 4:
+        printf("GALAT UTTAR\n");
+        printf("loser\n");
+        break;
+
+
+        case 2:
+        printf("SAHI JAWAB\n");
+        printf("You have won 2,000 rupees\n");
+        break;
+        default:printf("invalid ");
+        break;
+    }
+    printf("...................................................................\n");
+    printf("3RD sawal 5,000 rs k liye apki screen par ye raha\n");
+    printf("Question 3-->The language of Lakshadweep is?\n");
+    printf("and your options are\n");
+    printf(".....................................................................\n");
+    printf("A)Tamil\nB)Telegu\nC)Malayalam\nD)Hindi\n");
+    scanf("%d",&c);
+    switch(c)
+    {
+        case 1:
+        case 2:
+        case 4:
+        printf("GALAT UTTAR(xxxxxxxxx)\n");
+        printf("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
+        printf("LOSER\n");
+        break;
+
+        case  3:
+        printf("SAHI JAWAB\n");
+        printf("You have won 5,000 rupees\n");
+        break;
+        default:printf("/ninvalid ");
+        break;
+    }
+    printf("...................................................................");
+    printf("4th sawal 10,000 rs k liye apki screen par ye raha\n");
+    printf("Question 4-->How many minimum sundays can there be in a year?\n");
+    printf("and your options are\n");
+    printf(".....................................................................\n");
+    printf("1)42\n2)100\n3)365\n4)52\n");
+    scanf("%d",&c);
+    switch(c)
+    {
+        case 1:
+        case 2:
+        case 3:
+        printf("GALAT UTTAR(xxxxxxxxx)\n\n");
+        printf("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
+        printf("LOSER\n");
+        break;
+
+        case 4:
+        printf("SAHI JAWAB\n");
+        printf("CONGRATULATIONSS\n");
+        printf("You have won 10,000 rupees\n");
+        break;
+        default:printf("invalid ");
+        break;
+    }
+    printf("...................................................................\n");
+    printf("5th sawal 20,000 rs k liye apki screen par ye raha\n");
+    printf("Question 5-->What is the capital of ARGENTINA?\n");
+    printf("and your options are\n");
+    printf(".....................................................................\n");
+    printf("1)Manila\n2)muscat\n3)Buenus Aires\n4)Paris\n");
+    scanf("%d",&c);
+    printf("lock kardiya jaye\n");
+    switch(c)
+    {
+        case 1:
+        case 2:
+        case 4:
+        printf("GALAT UTTAR(xxxxxxxxx)\n");
+        printf("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
+        printf("LOSER\n");
+        break;
+
+        case 3:
+        printf("SAHI JAWAB\n");
+        printf("You have won 20,000 rupees\n");
+        break;
+        default:printf("/ninvalid ");
+        break;
+    }
+    
+
+    }
+    
